eb3507228c07c95cf418c7ff468eebb5852ee637
Zach Day <zachdayz@gmail.com>
2022-03-31 16:12:59 -0700

    Support for "home channels"

--- main.js
+++ main.js
@@ -8,34 +8,59 @@ const CHATS_DIR = 'chats';
 const files = fs.readdirSync(CHATS_DIR);
 
 const extraUserData = JSON.parse(stripComments(fs.readFileSync('userData.jsonc').toString()));
+const channelRelationTypes = [];
 const userStats = {};
+
+for (const [userId, userData] of Object.entries(extraUserData)) {
+    userStats[userId] = {
+        // We will find name when iterating thru messages.
+        name: null,
+        totalMessageCount: 0,
+        relationStats: []
+    };
+
+    if ('channels' in userData)
+        for (const channel of userData['channels'])
+            if (channelRelationTypes.indexOf(channel['relation']) === -1)
+                channelRelationTypes.push(channel['relation']);
+}
 
 for (let file of files) {
     const jsonParser = jsonStream.parse('messages.*.author');
-    let currentHomeChannelOwner = null;
+    let channelRelations = [];
 
     jsonParser.on('data', function(data) {
-        if (data['discriminator'] === '0000' || data['isBot'] === true)
+        const userId = data['id'];
+
+        if (data['isBot'] === true)
             return;
 
-        if (!(data['id'] in userStats)) {
-            userStats[data['id']] = {name: data['name'] + '#' + data['discriminator'], count: 0, homeChannelActivity: 0, ownHomeChannelMessages: 0};
+        if (!(userId in userStats)) {
+            console.warn(`Unknown user ${data['name']}`)
+            return;
         }
 
-        userStats[data['id']].count++;
-        if (currentHomeChannelOwner != null) {
-            userStats[currentHomeChannelOwner].homeChannelActivity++;
-            if (data['id'] === currentHomeChannelOwner)
-                userStats[currentHomeChannelOwner].ownHomeChannelMessages++;
+        if (userStats[userId].name == null)
+            userStats[userId].name = data['name'] + '#' + data['discriminator'];
+
+        userStats[userId].totalMessageCount++;
+
+        for (let channelRelation of channelRelations) {
+            if (!userStats[channelRelation.user].relationStats.some(rel => rel.relation === channelRelation.relation))
+                userStats[channelRelation.user].relationStats.push({ relation: channelRelation.relation, ownMessageCount: 0, totalMessageCount: 0 });
+        
+            const thisRelationStats = userStats[channelRelation.user].relationStats.find(rel => rel.relation === channelRelation.relation);
+            thisRelationStats.totalMessageCount++;
+            if (userId === channelRelation.user)
+                thisRelationStats.ownMessageCount++;
         }
     }, {});
 
     jsonParser.on('header', function(data) {
-        for (const [key, value] of Object.entries(extraUserData)) {
-            if (value['homeChannel'] == data['channel']['id']) {
-                currentHomeChannelOwner = key;
-                break;
-            }
+        for (const [userId, extraData] of Object.entries(extraUserData)) {
+            for (const channel of extraData['channels'])
+                if (channel['id'] === data['channel']['id'])
+                    channelRelations.push({ user: userId, relation: channel['relation']})
         }
     });
 
@@ -45,8 +70,21 @@ for (let file of files) {
 
 const outputFile = fs.createWriteStream('output.csv');
 
-outputFile.write('Name,Total Messages,All Messages in Home Channel,Own Messages in Home Channel\n')
+outputFile.write('Name,Total Messages');
+for (const relationName of channelRelationTypes)
+    outputFile.write(`,All Messages in ${relationName} Channel(s),Own Messages in ${relationName} Channel(s)`);
+outputFile.write('\n');
+
 for (let val of Object.values(userStats)) {
-    outputFile.write(`${val.name},${val.count},${val.homeChannelActivity},${val.ownHomeChannelMessages}\n`)
+    outputFile.write(`${val.name},${val.totalMessageCount}`);
+    for (const relationName of channelRelationTypes) {
+        if (val.relationStats.some(rel => rel.relation === relationName)) {
+            const thisRelationStats = val.relationStats.find(rel => rel.relation === relationName);
+            outputFile.write(`,${thisRelationStats.totalMessageCount},${thisRelationStats.ownMessageCount}`);
+        } else {
+            outputFile.write(',,');
+        }
+    }
+    outputFile.write('\n');
 }
 outputFile.close();
